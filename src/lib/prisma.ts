ba503824c98e93e6bfc3630daa7844b6548a2e5f
Seyi Adetunji <seyiadetunji@gmail.com>
2026-03-27 15:12:07 +0100

    feat(prisma): enable driverAdapters and Neon HTTP adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neon } from "@neondatabase/serverless";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const sql = neon(connectionString);
+const adapter = new PrismaNeon(sql);
+
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient();
+  new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
