242afc96e59db592ced83595e1b58cbc302de24e
Seyi Adetunji <seyiadetunji@gmail.com>
2026-03-31 08:10:33 +0100

    fix the shift errors

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaNeonHttp } from "@prisma/adapter-neon";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-// PrismaNeonHttp uses standard HTTPS (Port 443) to bypass local network blocks on Port 5432
-const adapter = new PrismaNeonHttp(connectionString, {});
-
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient({ adapter });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
